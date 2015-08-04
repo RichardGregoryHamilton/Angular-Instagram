b19c86365ca33f733f48e141c06f639cd755aeb1
Richard Hamilton <richardgregoryhamilton@gmail.com>
2015-08-04 12:55:14 -0500

    build login-form

--- client/controllers/login.js
+++ client/controllers/login.js
@@ -1,1 +1,28 @@
-// To Do
+angular.module('Instagram')
+	.controller('loginCtrl', function($scope, $window, $location, $rootScope, $auth) {
+		$scope.instagramLogin = function() {
+			$auth.authenticate('instagram')
+				.then(function(response) {
+					$window.localStorage.currentUser = JSON.stringify(response.data.user);
+					$rootScope.currentUser = JSON.parse($window.localStorage.currentUser);
+				})
+				.catch(function(response) {
+					console.log(response.data);
+				})
+		};
+		
+		$scope.emailLogin = function() {
+			$auth.login({ email: $scope.email, password: $scope.password })
+				.then(function(response) {
+					$window.localStorage.currentUser = JSON.stringify(response.data.user);
+					$rootScope.currentUser = JSON.parse($window.localStorage.currentUser)
+				})
+				.catch(function(response) {
+					$scope.errorMessages = {};
+					angular.forEach(response.data.message, function(message, field) {
+						$scope.loginForm[field].$setValidity('server', false);
+						$scope.errorMessage[field] = response.data.message[field];
+					});
+				});
+		};
+	})
