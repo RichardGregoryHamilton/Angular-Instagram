49b434751504d0fd4a98bdd5ab63c0319c55fbd2
Richard Hamilton <richardgregoryhamilton@gmail.com>
2015-08-04 21:34:47 -0500

    add Node for server side

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	db: 'localhost',
+	db: process.env.db || 'localhost',
 	clientSecret: process.env.clientSecret || "c5b0166fcddd408db69a287a32c1fb5e",
 	tokenSecret: process.env.tokenSecret || "63329b4f4c714f389318b8c3621550f8"
 };
