cd6bb66277c7ba24c955493596f147f1839389ba
Richard Hamilton <richardgregoryhamilton@gmail.com>
2015-08-05 11:18:38 -0500

    Add Instagram Authentication

--- client/controllers/signup.js
+++ client/controllers/signup.js
@@ -1,1 +1,14 @@
-// To Do
+angular.module('Instagram')
+	.controller('SignupCtrl', function($scope, $auth) {
+		$scope.signup = function() {
+			var user = {
+				email: $scope.email,
+				password: $scope.password
+			};
+			
+			$auth.signup(user)
+				.catch(function(response) {
+					console.log(response.data);
+				});
+		};
+	})
