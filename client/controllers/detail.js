b24c183e96c104be5930f49e13e4c255c50a963c
Richard Hamilton <richardgregoryhamilton@gmail.com>
2015-08-06 22:34:47 -0500

    Add gulp

--- client/controllers/detail.js
+++ client/controllers/detail.js
@@ -1,1 +1,16 @@
-// To Do
+angular.module('Instagram')
+	.controller('DetailCtrl', function($scope, $rootScope, $location, API) {
+		var mediaId = $location.path().split('/').pop();
+		
+		API.getMediaById(mediaId).success(function(media) {
+			$scope.hasLiked = media.user.has_liked;
+			$scope.photo = media;
+		});
+		
+		$scope.like = function() {
+			$scope.hasLiked = true;
+			API.likeMedia(mediaId).error(function(data) {
+				sweetAlert('Error', data.message, 'error');
+			});
+		};
+	});
--- client/controllers/navbar.js
+++ client/controllers/navbar.js
@@ -1,1 +1,11 @@
-// To Do
+angular.module('Instagram')
+	.controller('NavbarCtrl', function($scope, $rootScope, $window, $auth) {
+		$scope.isAuthenticated = function() {
+			return $auth.isAuthenticated();
+		};
+		
+		$scope.logout = function() {
+			$auth.logout();
+			delete $window.localStorage.currentUser;
+		};
+	});
